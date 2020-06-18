3c48fd620564ba6e139e47f859145b74044e7aad
ekim22 <ekim2@ggc.edu>
2020-06-17 20:30:44 -0400

    Corrected indent error in models.py. Registered entry with admin site.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -19,6 +19,6 @@ class Entry(models.Model):
     class Meta:
         verbose_name_plural = 'entries'
         
-        def __str__(self):
-            """Return a string representation of the model."""
-            return f"{self.text[:50]}..."
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
